a013e57d3757bef652eb6f1c4470e9d8960231a4
Richard A <richardaljasteabramson@gmail.com>
2016-02-27 21:40:52 +0200

    HTML & CSS Beautified + Smooth 2nd paragraph for bio card

---
+++ js/i2sujuv.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    setTimeout(function () {
+        $(document).ready(function () {
+            $('p.tv').stop(true).fadeIn({
+                duration: 2000
+                , queue: false
+            }).css('display', 'none').slideDown(1000);
+        });
+    }, 3000);
+});
