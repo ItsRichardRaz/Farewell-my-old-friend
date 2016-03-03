13ba8b726b21e24d124e48c02774ac7013633c47
Richard A <richardaljasteabramson@gmail.com>
2016-03-03 10:13:30 +0200

    Sponsor logo hover JS fallback

---
+++ js/sponsorFB.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', 'http://richard.ee/img/zone-color.png');
+}
+
+function unhover(element) {
+    element.setAttribute('src', 'http://richard.ee/img/zone-gray.png');
+}
